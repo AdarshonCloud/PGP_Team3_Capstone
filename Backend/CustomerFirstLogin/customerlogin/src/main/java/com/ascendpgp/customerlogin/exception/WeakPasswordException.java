f970f854548edafdf42b91192595a9f057abec9c
Adarsh Sharma <143775652+AdarshonCloud@users.noreply.github.com>
2024-12-25 01:31:58 +0530

    Delete Backend/CustomerFirstLogin/customerlogin directory

--- Backend/CustomerFirstLogin/customerlogin/src/main/java/com/ascendpgp/customerlogin/exception/WeakPasswordException.java
+++
@@ -1,9 +0,0 @@
-package com.ascendpgp.customerlogin.exception;
-
-public class WeakPasswordException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public WeakPasswordException(String message) {
-        super(message);
-    }
-}
