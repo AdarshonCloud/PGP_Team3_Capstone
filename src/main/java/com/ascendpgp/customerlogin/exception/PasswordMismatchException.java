194676314a3d053a4818daac610d9bd62b037abc
Adarsh Sharma <143775652+AdarshonCloud@users.noreply.github.com>
2025-01-02 22:16:19 +0530

    Delete src directory

--- src/main/java/com/ascendpgp/customerlogin/exception/PasswordMismatchException.java
+++
@@ -1,9 +0,0 @@
-package com.ascendpgp.customerlogin.exception;
-
-public class PasswordMismatchException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public PasswordMismatchException(String message) {
-        super(message);
-    }
-}
